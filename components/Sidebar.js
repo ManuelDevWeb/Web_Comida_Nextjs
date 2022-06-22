8b0c8a27f578d1e69fd8cc22bbefd2aad8488188
ManuelDev <manueldeveloper17@gmail.com>
2022-06-22 13:35:46 -0500

    Se creo el componente SideBar y Categoria

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
+// Custom hooks
+import useQuiosco from "../hooks/useQuisco";
+// Components
+import Categoria from "./Categoria";
+
 const Sidebar = () => {
+  // Accediendo a los datos del contexto Quiosco a traves del custom hook useQuiosco()
+  const { categorias } = useQuiosco();
+
   return (
     <>
       <Image
@@ -9,6 +17,15 @@ const Sidebar = () => {
         src={"/assets/img/logo.svg"}
         alt="imagen logotipo"
       />
+
+      <nav className="mt-10">
+        {
+          // Iterando sobre categorias
+          categorias.map((categoria) => (
+            <Categoria key={categoria.id} categoria={categoria} />
+          ))
+        }
+      </nav>
     </>
   );
 };
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,12 +1,31 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 // Creando el contexto (Permite acceder al contexto)
 const QuioscoContext = createContext();
 
 // Provider (Permite a los hijos tener acceso al state)
 const QuioscoProvider = ({ children }) => {
+  // State para manejar las categorias
+  const [categorias, setCategorias] = useState([]);
+
+  // UseEffect que se ejecuta cuando carga el componente
+  useEffect(() => {
+    obtenerCategorias();
+  }, []);
+
+  // Funcion para consultar las categorias de la API
+  const obtenerCategorias = async () => {
+    // Obteniendo las categorias
+    const { data } = await axios.get("/api/categorias");
+    // Actualizamos el state categorias
+    setCategorias(data);
+  };
+
   return (
-    <QuioscoContext.Provider value={{}}>{children}</QuioscoContext.Provider>
+    <QuioscoContext.Provider value={{ categorias }}>
+      {children}
+    </QuioscoContext.Provider>
   );
 };
 
