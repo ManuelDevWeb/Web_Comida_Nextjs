778d8080076f903392fb75418c8888abe83b0232
ManuelDev <manueldeveloper17@gmail.com>
2022-08-06 23:53:39 -0500

    Update component total

--- pages/total.js
+++ pages/total.js
@@ -5,7 +5,7 @@ export default function Total() {
   return (
     <Layout pagina="Total y Confirmar Pedido">
       <h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
-      <p className="text-2xl my-5">Confirma tu Pedido a Continuacion</p>
+      <p className="text-2xl my-5 mb-2">Confirma tu Pedido a Continuacion</p>
     </Layout>
   );
 }
